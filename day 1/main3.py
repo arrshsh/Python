74b615be09a3f2f4192d242f8d2857895f7b3199
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-11 23:01:27 +0530

    Add files via upload

---
+++ day 1/main3.py
@@ -0,0 +1,5 @@
+# Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string.
+
+name = str(input("What is your name??"))
+length = len(name)
+print(length)
