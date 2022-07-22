37d5f8d05602cbeab8dca51cf24a85d12b8b62c3
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-22 14:36:29 +0530

    Update main4.py

--- day 3/main4.py
+++ day 3/main4.py
@@ -1,1 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
 
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+var_1 = input ("You are at a crossroad. Were would you like to go? Left or Right?")
+var_1_final = var_1.lower()
+if var_1_final == "left":
+    var_2 = input ("You are at a river.Would you like to swim or wait??")
+else:
+    print("You fell into hole, GAME OVER")
+
+var_2_final = var_2.lower()
+if var_2_final == "wait":
+    var_3 = input ("You have three doors: Red, Yellow Blue.\nWhich one whould you like to go through?")
+
+else:
+    print ("You were attacked. GAME OVER")
+
+var_3_final = var_3.lower()
+if var_3_final == "yellow":
+    print ("Congratulations, you win the game")
+else:
+    print ("You died. GAME OVER")
