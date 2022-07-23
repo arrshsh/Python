25321f76d123409e70015dc1ddb44b5a3d3aafd8
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-23 11:51:23 +0530

    Update main1.py

--- day 4/main1.py
+++ day 4/main1.py
@@ -1,1 +1,9 @@
+#Write your code below this line 👇
+#Hint: Remember to import the random module
+import random
+coin = random.randint(0,2)
+if coin == 1:
+    print ("Heads")
 
+else:
+    print ("Tails")
