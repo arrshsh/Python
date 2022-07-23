1726e615adb6c63aaf9672287b12745699560cd5
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-23 11:49:55 +0530

    Update main5.py

--- day 3/main5.py
+++ day 3/main5.py
@@ -1,1 +1,31 @@
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
 
+#Write your code below this line 👇
+
+size_final = size.lower()
+add_pepperoni_final = add_pepperoni.lower()
+extra_cheese_final = extra_cheese.lower()
+
+cost = 0
+if size_final == "s":
+    cost = 15
+    if add_pepperoni_final == "y":
+        cost += 2
+elif size_final == "m":
+    cost = 20
+    if add_pepperoni_final == "y":
+        cost += 3
+else:
+    cost = 25
+    if add_pepperoni_final == "y":
+        cost += 3
+
+if extra_cheese_final  == "y":
+    cost += 1
+
+print(f"Your final bill is ${cost}")
