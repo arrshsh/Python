8fbe14a8ac3e8649a2fcd0ebb566099d8319b65a
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-25 22:41:18 +0530

    Update main2.py

--- day 4/main2.py
+++ day 4/main2.py
@@ -1,1 +1,14 @@
+# Split string method
+import random
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+count = len (names)
+count -= 1
+rand = random.randint (0, count)
+print (f"{names[rand]} is going to pay the bill!!")
+
+ 
 
