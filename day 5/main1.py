718a360f4e00fbe884df9ae12013640ef1ea3fd6
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-07-26 22:07:07 +0530

    Update main1.py

--- day 5/main1.py
+++ day 5/main1.py
@@ -1,1 +1,20 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+
+#Write your code below this row 👇
+sum = 0
+for n in range (0, len(student_heights)):
+    sum += student_heights[n-1]
+
+average = sum / len(student_heights)
+average_final = round(average, 0)
+print(average_final)
+
+
+
+
 
