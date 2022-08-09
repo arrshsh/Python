a59994a969cb3afe162d8c92c4fbdd4920a37390
arrshsh <84338598+arrshsh@users.noreply.github.com>
2022-08-09 23:19:42 +0530

    Update main2.py

--- day 8/main2.py
+++ day 8/main2.py
@@ -1,1 +1,23 @@
+#Write your code below this line 👇
+import math
+def prime_checker(number):
+  for n in range(2, int (math.sqrt(number))+1):
+    if number % n == 0:
+      print ("Not a prime number")
+      break
+    else:
+      print ("It is a prime number")
+      break
+
+
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
+
+
+
 
